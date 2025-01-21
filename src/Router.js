69d005dad7a82811734a9bfffd23d3eedbb109fb
CyrellEstrada <cyrellestrada1@gmail.com>
2025-01-20 20:05:19 -0600

    Removed Dashboard that was making hosted frontend fail

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,10 @@
-// import ReactDOM from "react-dom/client";
 import { Routes, Route } from "react-router";
 import Home from "./components/Home";
-import Dashboard from "./components/Dashboard";
 
 const Router = () => {
   return (
       <Routes>
         <Route path="/" element={<Home />}/>
-          <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
   );
 }
