2a021cbe74ba7d6ef8ede6617dd8d6803987e4b1
CyrellEstrada <cyrellestrada1@gmail.com>
2025-01-24 12:32:09 -0600

    Implemented a budget dashboard for users

---
+++ src/components/BudgetDashboard.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState} from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Link } from "react-router";
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Paper,
+    Tab
+} from '@mui/material'
+import BasicModal from './Modal'
+
+
+const BudgetDashboard = () => {
+    const [budgets, setBudgets] = useState([])
+    const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
+
+    useEffect(() => {
+        const fetchBudgets = async () => {
+            try {
+                if (!isAuthenticated) return
+                const token = await getAccessTokenSilently
+                const res = await fetch(
+                    `http://localhost:4001/budget/${user.sub}`,
+                    {
+                        headers: {
+                            Authorization: 'Bearer ${token}'
+                        }
+                    }
+                )
+                if (!res.ok) {
+                    throw new Error(`Server error: ${res.status}`)
+                }
+
+                const data = await res.json()
+                setBudgets(data)
+            } catch (error) {
+                console.error('Failed to fetch budgets:', error)
+            }
+        }
+        fetchBudgets()
+    }, [isAuthenticated, user, getAccessTokenSilently])
+    
+    return (
+        // GET request for user's budget using getAllBudgetsbyUserId or getBudgetById in router backend, use endpoint in router not functions in controllers
+        
+        //Information that come from GET request save in a variable or state, use MUI element or table, goes in return statment below
+        <>
+        <h1>Welcome to your Dashboard</h1>
+                {/* HTML Form to enter information or MUI Form*/}
+        <TableContainer component={Paper} sx={{maxWidth: 600, margin: 'auto', mt: 4}}>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Budget ID</TableCell>
+                        <TableCell>User ID</TableCell>
+                        <TableCell>Savings Goal</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                <TableRow sx={{textAlign:'right'}}>
+                    <TableCell >{<BasicModal />}</TableCell>
+                </TableRow>
+                    {budgets.map((budget) => (
+                        <TableRow key={budget.id}>
+                            <TableCell>{budget.id}</TableCell>
+                            <TableCell>{budget.user_id}</TableCell>
+                            <TableCell>{budget.savings_goal}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+</>
+    )
+}
+
+export default BudgetDashboard
